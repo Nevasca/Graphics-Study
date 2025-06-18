6685d1141784c3ecc7e4d5f696267b1c5684843b
Bruno Sanches <bruno_sanches15@hotmail.com>
2025-06-18 08:45:26 -0300

    feat: implement simple vertex shader

---
+++ Studies/DX12Study/data/color.hlsl
@@ -0,0 +1,38 @@
+﻿// HLSL = high level shading language
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorldViewProj;
+};
+
+struct VertexIn
+{
+    // the attached semantics ": POSITION" and ": COLOR"
+    // must match the semantic specified by the D3D12_INPUT_ELEMENT_DESC, so we can match vertex elements to shader elements
+    float3 PosLocal : POSITION; // Vertex local position 
+    float4 Color : COLOR;
+};
+
+struct VertexOut
+{
+    // SV stands for System Value. There are some special semantics that GPU needs
+    // SV_POSITION holds the vertex position in homogeneous clip space. GPU needs to be aware of this value
+    // to involve in other operations, such as clipping, depth testing, rasterization.
+    // If we use a geometry shader, the job of outputting the homogeneous clip space position can be deferred to the geometry shader
+    float4 PosHomogeneousClip : SV_POSITION;
+    float4 Color : COLOR;
+};
+
+// We could have also not used structs and place all parameters on the VS function and using out for output values for next stage, like so:
+// void VS(float3 iPosL : POSITION, float3 iColor : COLOR, out float4 oPosH : SV_POSITION, out float4 oColor : COLOR)
+// Using structs is much more organized though
+VertexOut VS(VertexIn vertexIn)
+{
+    VertexOut vertexOut;
+
+    vertexOut.PosHomogeneousClip = mul(float4(vertexIn.PosLocal, 1.f), gWorldViewProj);
+
+    vertexOut.Color = vertexIn.Color;
+    
+    return vertexOut;
+}
