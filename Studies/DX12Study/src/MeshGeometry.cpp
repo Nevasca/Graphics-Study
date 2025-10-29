cf62c388d27bf4c8d94567491ac7b0066aa72c07
Bruno Sanches <bruno_sanches15@hotmail.com>
2025-10-29 08:26:36 -0300

    feat: implement MeshGeometry helper structure

---
+++ Studies/DX12Study/src/MeshGeometry.cpp
@@ -0,0 +1,24 @@
+﻿#include "MeshGeometry.h"
+
+namespace Studies
+{
+    D3D12_VERTEX_BUFFER_VIEW MeshGeometry::GetVertexBufferView() const
+    {
+        D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+        vertexBufferView.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
+        vertexBufferView.StrideInBytes = VertexByteStride;
+        vertexBufferView.SizeInBytes = VertexBufferByteSize;
+
+        return vertexBufferView;
+    }
+
+    D3D12_INDEX_BUFFER_VIEW MeshGeometry::GetIndexBufferView() const
+    {
+        D3D12_INDEX_BUFFER_VIEW indexBufferView{};
+        indexBufferView.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
+        indexBufferView.Format = IndexFormat;
+        indexBufferView.SizeInBytes = IndexBufferByteSize;
+
+        return indexBufferView;
+    }
+}
---
+++ Studies/DX12Study/src/MeshGeometry.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <d3d12.h>
+#include <d3dcommon.h>
+#include <DirectXCollision.h>
+#include <string>
+#include <unordered_map>
+#include <wrl/client.h>
+
+namespace Studies
+{
+    struct SubmeshGeometry
+    {
+        UINT IndexCount{0};
+        UINT StartIndexLocation{0};
+        INT BaseVertexLocation{0};
+
+        // Used in later chapters of the book
+        DirectX::BoundingBox Bounds{};
+    };
+    
+    struct MeshGeometry
+    {
+        std::string Name{};
+
+        // System memory copies, using Blobs because the vertex/index format can be generic
+        // It's up to the client to cast appropriately
+        Microsoft::WRL::ComPtr<ID3DBlob> VertexBufferCPU{nullptr};
+        Microsoft::WRL::ComPtr<ID3DBlob> IndexBufferCPU{nullptr};
+
+        Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferGPU{nullptr};
+        Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferGPU{nullptr};
+
+        Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferUploader{nullptr};
+        Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferUploader{nullptr};
+
+        UINT VertexByteStride{0};
+        UINT VertexBufferByteSize{0};
+        DXGI_FORMAT IndexFormat{DXGI_FORMAT_R16_UINT};
+        UINT IndexBufferByteSize{0};
+
+        // A MeshGeometry may store multiple geometries in one vertex/index buffer
+        std::unordered_map<std::string, SubmeshGeometry> DrawArgs{};
+
+        D3D12_VERTEX_BUFFER_VIEW GetVertexBufferView() const;
+        D3D12_INDEX_BUFFER_VIEW GetIndexBufferView() const;
+    };
+}
