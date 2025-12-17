6110a8a5363b4a4c208003d9a9d7b5c63556c2e7
Bruno Sanches <bruno_sanches15@hotmail.com>
2025-12-17 15:00:24 -0300

    feat: add PassConstants members

--- Studies/DX12Study/src/PassConstants.h
+++ Studies/DX12Study/src/PassConstants.h
@@ -1,9 +1,24 @@
 ﻿#pragma once
+#include <DirectXMath.h>
 
 namespace Studies
 {
+    // Constant data that is fixed over a given rendering pass
     struct PassConstants
     {
-
+        DirectX::XMFLOAT4X4 View;
+        DirectX::XMFLOAT4X4 InvView;
+        DirectX::XMFLOAT4X4 Proj;
+        DirectX::XMFLOAT4X4 InvProj;
+        DirectX::XMFLOAT4X4 ViewProj;
+        DirectX::XMFLOAT4X4 InvViewProj;
+        DirectX::XMFLOAT3 EyePositionWorld;
+        float CbPerObjectPad1;
+        DirectX::XMFLOAT2 RenderTargetSize;
+        DirectX::XMFLOAT2 InvRenderTargetSize;
+        float NearZ;
+        float FarZ;
+        float TotalTime;
+        float DeltaTime;
     };
 }
