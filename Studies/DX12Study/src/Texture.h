49874859e2c86c33ee5d5931dee9400b9639883d
Bruno Sanches <bruno_sanches15@hotmail.com>
2026-02-10 08:16:02 -0300

    feat: implement Texture class

---
+++ Studies/DX12Study/src/Texture.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+#include <d3d12.h>
+#include <wrl/client.h>
+
+namespace Studies
+{
+    class Texture
+    {
+    public:
+        std::string Name; // Unique texture name for lookup
+        std::wstring FileName;
+        Microsoft::WRL::ComPtr<ID3D12Resource> Resource{nullptr};
+        Microsoft::WRL::ComPtr<ID3D12Resource> UploadHeapResource{nullptr};
+    };
+}
